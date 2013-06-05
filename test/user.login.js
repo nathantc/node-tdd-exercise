5f13659d6b2cc5fdb63bd68098bfd094d106b927
Nathan <nathantc@gmail.com>
2013-06-04 22:35:09 -0400

    Testing bcrypt compare hash functionality on heroku.

--- test/user.login.js
+++ test/user.login.js
@@ -1,6 +1,6 @@
 var user = require('../lib/user'),
     userStore = require('../lib/userStore'),
-    bcrypt = require('bCrypt'),
+    bcrypt = require('bcrypt'),
     assert = require('assert'),
     sinon = require('sinon');
 
